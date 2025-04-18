b5ab09efc285c4f4368bfc427bcc82e47a138c13
Ted <t.aldentempleman@gmail.com>
2025-04-18 01:13:38 +0100

    Convert starting balance to user's currency

--- webapps2025/register/models.py
+++ webapps2025/register/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from payment.helpers import convert_currency
+
 STARTING_BALANCE = 750.00
 CURRENCY_OPTIONS = [
     ("EUR", "Euros"),
@@ -20,6 +22,14 @@ class CustomUser(AbstractUser):
         decimal_places=2,
         default=STARTING_BALANCE,
     )
+
+    def save(self, *args, **kwargs):
+        # Set default balance only when creating a new user, i.e. if the
+        # user has no primary key (ID) yet.
+        if not self.pk:
+            # Convert the starting balance to the user's currency.
+            self.balance = convert_currency("GBP", self.currency, STARTING_BALANCE)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.email} ({self.currency})"
