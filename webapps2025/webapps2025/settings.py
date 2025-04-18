c87144757f3332b57244b6257f8dd2c71cc76ad0
Ted <t.aldentempleman@gmail.com>
2025-04-18 10:11:53 +0100

    Update allowed hosts

--- webapps2025/webapps2025/settings.py
+++ webapps2025/webapps2025/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5+imat4r+(0=fukj3fqlhu_@enn8d8(2=vlrd3**p&9r-co4%3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
